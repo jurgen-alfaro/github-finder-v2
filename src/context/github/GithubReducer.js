e4fbaafea0b1e5d9001df096cc9cb3ed1c0bc724
jurgen-alfaro <dev.jurgen.alfaro@gmail.com>
2022-02-15 23:10:16 -0600

    App functionality completed

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -11,6 +11,14 @@ const githubReducer = (state, action) => {
         users: action.payload,
         isLoading: false,
       };
+    case "GET_USER_AND_REPOS":
+      return {
+        ...state,
+        user: action.payload.user,
+        repos: action.payload.repos,
+        isLoading: false,
+      };
+
     case "SET_LOADING":
       return {
         ...state,
